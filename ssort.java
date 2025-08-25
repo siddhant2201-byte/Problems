2b42186aa7a96fcbb14d491b91772fc21e23a83f
siddhant2201-byte <siddhant2201@gmail.com>
2025-08-25 22:30:36 +0530

    Create ssort.java

---
+++ ssort.java
@@ -0,0 +1,24 @@
+public class ssort {
+    public static void main(String[] args) {
+        int arr[] ={64,23,12,74,3};
+
+        selectionsort(arr);
+
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+    public static void selectionsort(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
